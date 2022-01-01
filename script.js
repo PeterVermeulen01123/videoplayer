9a0aecb7340a41c180ba74c46cee8ec7538b8b14
PeterVermeulen01123 <94803849+PeterVermeulen01123@users.noreply.github.com>
2022-01-01 16:31:53 -0500

    initial file upload to repo

---
+++ script.js
@@ -0,0 +1,184 @@
+
+
+const player = document.querySelector('.player');
+const video= document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
+const speed = document.querySelector('.player-speed');
+const expandBtn = document.getElementById('fs-btn');
+
+
+// Play & Pause ----------------------------------- //
+
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
+
+function togglePlay() {
+    if(video.paused) {
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    }else{
+        video.pause();
+        playBtn.classList.replace('fa-pause', 'fa-play');
+        playBtn.setAttribute('title', 'Play');
+        showPlayIcon();
+    }
+}
+
+//on video end, show play button icon
+video.addEventListener('ended', showPlayIcon);
+
+
+// Progress Bar ---------------------------------- //
+
+//calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time/60);
+    let seconds = Math.floor(time % 60);
+        seconds < 10 
+        ? seconds= `0${seconds}`
+        : seconds ;
+    return `${minutes}:${seconds}`;
+}
+
+//update progress bar as video plays
+function updateProgress() {
+  // console.log('currentTime: ', video.currentTime, 'duration: ', video.duration);
+  progressBar.style.width = `${ (video.currentTime/video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} / `;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
+
+//click to seek within video
+function setProgress(e) {
+    newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime*100}%`;
+    video.currentTime = newTime*video.duration;
+   }
+
+// Volume Controls --------------------------- //
+let lastVolume = 1;
+
+
+//volume bar
+function changeVolume(e) {
+    let volume = e.offsetX/volumeRange.offsetWidth;
+    //round the volume up or down
+    if(volume < 0.07 ){
+        volume = 0;
+    }
+    if(volume >0.9) {
+        volume = 1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+   // console.log(volume);
+    //change audio icon depending on volume level
+    volumeIcon.className = '';
+    if(volume > 0.7) {
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+    }else if(volume<0.9 && volume>0) {
+        volumeIcon.classList.add('fas', 'fa-volume-down');
+    }else if(volume===0.) {
+        volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+    lastVolume = volume;
+}
+
+//mute un/mute
+function toggleMute() {
+    volumeIcon.className = '';
+    if(video.volume) {
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add('fas', 'fa-volume-mute');
+        volumeIcon.setAttribute('title', 'Unmite');
+    }else{ //if it was muted
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`;
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+        volumeIcon.setAttribute('title', 'Mute');
+    }
+}
+
+// Change Playback Speed -------------------- //
+
+function changeSpeed() {
+  video.playbackRate = speed.value;
+}
+
+// Fullscreen ------------------------------- //
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+    video.classList.add('video-fullscreen');
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+    video.classList.remove('video-fullscreen');
+  }
+
+let fullscreen = false;
+//toggle fullscreen
+function toggleFullscreen() {
+    if(document.fullscreenElement){//document is full screen
+        closeFullscreen();
+        video.classList.remove('video-fullscreen');
+        video.classList.add('video');
+        fullscreen = false;
+        expandBtn.classList.replace('fa-compress', 'fa-expand');
+        expandBtn.setAttribute('title', 'Expand');
+    }else{//document is not full screen
+        openFullscreen(player);
+        video.classList.add('video-fullscreen');
+        video.classList.remove('video');
+        fullscreen = true;
+        expandBtn.classList.replace('fa-expand', 'fa-compress');
+        expandBtn.setAttribute('title', 'Compress');
+    }
+}
+
+//event listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
+fullscreenBtn.addEventListener('click', toggleFullscreen);
+
+document.addEventListener('fullscreenchange', (event) => {
+     if(!document.fullscreenElement){ //check if it is full screen now
+        // if its false... make sure the video styling removes full-screen class
+        video.classList.remove('video-fullscreen');
+        video.classList.add('video');
+    }
+  });
